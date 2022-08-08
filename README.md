6418bed203b054f459793c5cf3af17ef5d34fe93
Ahang <jiacheng_niii@outlook.com>
2022-08-08 23:09:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# what is this?
+
+Get perfect shadows every time  for the non-designer.
+
+# Installation
+
+`npm i shadowizard-jiacheng --save`
+
+
+Then ...
+
+```
+import { shadowizard } from 'shaowizard'
---
+++ index.js
@@ -0,0 +1,18 @@
+function shadowizeard(options) {
+  let images = document.querySelectorAll(".shadowizeard");
+
+  if (options.shadowizeard_type === "hard") {
+    options.shadowizeard_type = "0px";
+  } else {
+    options.shadowizeard_type = "15px";
+  }
+
+  images.forEach((image) => {
+    image.style.boxShadow = `10px 10px ${options.shadowizeard_type} 1px rgba(0,0,0,0.12)`;
+
+    if (options.padding) {
+      image.style.padding = "1em";
+    }
+  });
+}
+modules.exports.shadowizeard = shadowizeard;
